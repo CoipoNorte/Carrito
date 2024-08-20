a8baea19e392a5126df01197487e458dfb1daad5
CoipoNorte <christiancaceres1398@gmail.com>
2024-08-20 03:40:21 -0400

    .

--- src/components/product.js
+++ src/components/product.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Button, CardText, CardSubtitle, CardTitle, CardBody, CardImg, Col, Card } from 'reactstrap';
+import { CardText, CardSubtitle, CardTitle, CardBody, CardImg, Col, Card } from 'reactstrap';
 import './product.css';
+import ProductSheet from './productsheet'
 
-function Product(props) {
-    return (
-        <Col sm="4">
-            <Card className='Card' body outline color='primary'>
-                <CardImg src={props.img} />
-                <CardBody>
-                    <CardTitle>{props.title}</CardTitle>
-                    <CardSubtitle><b>Valor: $ </b> {props.subtitle} </CardSubtitle>
-                    <CardText> {props.text} </CardText>
-                    <Button className='Button'>Comprar</Button>
-                    <Button className='Button'>Ver ficha</Button>
-                </CardBody>
-            </Card>
-        </Col>
-    );
+class Product extends React.Component {
+    render() {
+        return (
+            <Col sm="4">
+                <Card className='Card' body outline color='primary'>
+                    <CardImg src={this.props.img} />
+                    <CardBody>
+                        <CardTitle>{this.props.title}</CardTitle>
+                        <CardSubtitle><b>Valor: $ </b> {this.props.subtitle} </CardSubtitle>
+                        <CardText> {this.props.text} </CardText>
+                        <ProductSheet props={this.props}/>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
 }
 
 export default Product;
